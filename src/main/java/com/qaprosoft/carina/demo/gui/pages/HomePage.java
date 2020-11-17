7a491ea89c4da2e4a562af8d3a9868283a3b2fce
okamara <32964663+okamara@users.noreply.github.com>
2020-11-17 16:50:14 +0300

    Update HomePage.java

--- src/main/java/com/qaprosoft/carina/demo/gui/pages/HomePage.java
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/HomePage.java
@@ -35,15 +35,11 @@ public class HomePage extends AbstractPage {
 
     @FindBy(xpath = "//div[contains(@class, 'brandmenu-v2')]//a")
     private List<ExtendedWebElement> brandLinks;
-    private String st = null;
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
     public FooterMenu getFooterMenu() {
-     if (st.isEmpty()) {
-   System.out.println("EMPTY");
-   } 
         return footerMenu;
     }
 
